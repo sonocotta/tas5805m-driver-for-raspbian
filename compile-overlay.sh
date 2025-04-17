44dcb9f55240672252c6bca9159b0ec68f858214
andriy.malyshenko <andriy@sonocotta.com>
2025-04-17 22:42:34 +0200

    namings

--- compile-overlay.sh
+++ compile-overlay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dtc -I dts -O dtb -o /boot/overlays/tas5805m.dtbo tas5805m-overlay.dts
-dtc -I dts -O dtb -o /boot/overlays/tas5805m-duo.dtbo tas5805m-duo-overlay.dts
+dtc -I dts -O dtb -o /boot/overlays/tas5805m-1x.dtbo tas5805m-1x-overlay.dts
+dtc -I dts -O dtb -o /boot/overlays/tas5805m-2x.dtbo tas5805m-2x-overlay.dts
